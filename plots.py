eab7dd5eeb34bdd5b9719872d04d8b3321a427f6
hqin-2020 <uctphqi@ucl.ac.uk>
2023-07-13 11:34:10 -0500

    plot

--- plots.py
+++ plots.py
@@ -25,9 +25,9 @@ parser.add_argument("--sitenum",type=int,default=10)
 parser.add_argument("--time",type=int,default=200)
 parser.add_argument("--dataname",type=str,default="tests")
 parser.add_argument("--mix_in",type=int,default=2)
-parser.add_argument("--mass_matrix",type=float,default=1)
+parser.add_argument("--mass_matrix_theta",type=float,default=1)
+parser.add_argument("--mass_matrix_gamma",type=float,default=1.0)
 parser.add_argument("--symplectic_integrator_num_steps",type=int,default=2)
-
 
 args = parser.parse_args()
 weight = args.weight
@@ -40,12 +40,13 @@ time = args.time
 xi = args.xi
 dataname = args.dataname
 mix_in= args.mix_in
-mass_matrix=args.mass_matrix
+mass_matrix_theta=args.mass_matrix_theta
+mass_matrix_gamma=args.mass_matrix_gamma
 symplectic_integrator_num_steps=args.symplectic_integrator_num_steps
 
 workdir = os.getcwd()
-output_dir = workdir+"/output/"+dataname+"/pf_"+str(pf)+"_pa_"+str(pa)+"_time_"+str(time)+"/theta_"+str(theta_multiplier)+"_gamma_"+str(gamma_multiplier)+"/sitenum_"+str(sitenum)+"_xi_"+str(xi)+"/mix_in_"+str(mix_in)+"_mass_matrix_"+str(mass_matrix)+"_symplectic_integrator_num_steps_"+str(symplectic_integrator_num_steps)+"/weight_"+str(weight)+"/"
-plotdir = workdir+"/plot/"+dataname+"/pf_"+str(pf)+"_pa_"+str(pa)+"_time_"+str(time)+"/theta_"+str(theta_multiplier)+"_gamma_"+str(gamma_multiplier)+"/sitenum_"+str(sitenum)+"_xi_"+str(xi)+"/mix_in_"+str(mix_in)+"_mass_matrix_"+str(mass_matrix)+"_symplectic_integrator_num_steps_"+str(symplectic_integrator_num_steps)+"/weight_"+str(weight)+"/"
+output_dir = workdir+"/output/"+dataname+"/pf_"+str(pf)+"_pa_"+str(pa)+"_time_"+str(time)+"/theta_"+str(theta_multiplier)+"_gamma_"+str(gamma_multiplier)+"/sitenum_"+str(sitenum)+"_xi_"+str(xi)+"/mix_in_"+str(mix_in)+"_mass_matrix_theta_"+str(mass_matrix_theta)+"_mass_matrix_gamma_"+str(mass_matrix_gamma)+"_symplectic_integrator_num_steps_"+str(symplectic_integrator_num_steps)+"/weight_"+str(weight)+"/"
+plotdir = workdir+"/plot/"+dataname+"/pf_"+str(pf)+"_pa_"+str(pa)+"_time_"+str(time)+"/theta_"+str(theta_multiplier)+"_gamma_"+str(gamma_multiplier)+"/sitenum_"+str(sitenum)+"_xi_"+str(xi)+"/mix_in_"+str(mix_in)+"_mass_matrix_theta_"+str(mass_matrix_theta)+"_mass_matrix_gamma_"+str(mass_matrix_gamma)+"_symplectic_integrator_num_steps_"+str(symplectic_integrator_num_steps)+"/weight_"+str(weight)+"/"
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 if not os.path.exists(plotdir):
@@ -133,7 +134,7 @@ plt.title(r"Trace Plot of Um")
 legend = plt.legend(bbox_to_anchor=(1.05, 0.5), loc="center left", borderaxespad=0)
 fig.tight_layout()
 plt.subplots_adjust(right=0.7)
-axes.get_legend().remove()
+# axes.get_legend().remove()
 fig.savefig(plotdir +'Um.png', bbox_extra_artists=(legend,), bbox_inches='tight', dpi = 100)
 plt.close()
 
@@ -142,7 +143,7 @@ plt.plot(results['sol_val_Up_tracker'][-1], label=r"$Up_{%d}$"%(j+1))
 plt.xlabel("Iteration")
 plt.ylabel(r"$Up$")
 plt.title(r"Trace Plot of Up")
-axes.get_legend().remove()
+# axes.get_legend().remove()
 legend = plt.legend(bbox_to_anchor=(1.05, 0.5), loc="center left", borderaxespad=0)
 fig.tight_layout()
 plt.subplots_adjust(right=0.7)
@@ -154,7 +155,7 @@ plt.plot(results['sol_val_Z_tracker'][-1], label=r"$Z_{%d}$"%(j+1))
 plt.xlabel("Iteration")
 plt.ylabel(r"$Z$")
 plt.title(r"Trace Plot of Z")
-axes.get_legend().remove()
+# axes.get_legend().remove()
 legend = plt.legend(bbox_to_anchor=(1.05, 0.5), loc="center left", borderaxespad=0)
 fig.tight_layout()
 plt.subplots_adjust(right=0.7)
