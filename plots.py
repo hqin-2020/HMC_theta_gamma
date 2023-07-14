d8e43e054d58e83729fd8d237eefe8ae8596247e
hqin <45484238+hqin-2020@users.noreply.github.com>
2023-07-13 23:32:25 -0500

    Add files via upload

--- plots.py
+++ plots.py
@@ -5,12 +5,16 @@
 import os, sys
 import pickle
 import time
-
+import numpy as np
+import scipy.stats as stats
 import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set(font_scale=1.2)
 # Import the solvers
 import solvers
+
+sys.path.append(os.path.abspath("src"))
+from data_handlers import load_site_data
 
 ########################################################################
 import argparse
@@ -51,6 +55,18 @@ if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 if not os.path.exists(plotdir):
     os.makedirs(plotdir)
+
+
+(    zbar_2017,
+    gamma,
+    gammaSD,
+    z_2017,
+    forestArea_2017_ha,
+    theta,
+    thetaSD,
+) = load_site_data(sitenum, norm_fac=1e9, )
+
+
 
 with open(output_dir+'results.pcl', 'rb') as f:
     # Load the data from the file
@@ -126,63 +142,98 @@ plt.close()
 # fig.savefig(plotdir +'Ua.png', bbox_extra_artists=(legend,), bbox_inches='tight', dpi = 100)
 # plt.close()
 
-fig, axes = plt.subplots(1, 1, figsize = (8,6))
-plt.plot(results['sol_val_Um_tracker'][-1], label=r"$Um_{%d}$"%(j+1))
-plt.xlabel("Iteration")
-plt.ylabel(r"$Um$")
-plt.title(r"Trace Plot of Um")
-legend = plt.legend(bbox_to_anchor=(1.05, 0.5), loc="center left", borderaxespad=0)
-fig.tight_layout()
-plt.subplots_adjust(right=0.7)
-# axes.get_legend().remove()
-fig.savefig(plotdir +'Um.png', bbox_extra_artists=(legend,), bbox_inches='tight', dpi = 100)
-plt.close()
-
-fig, axes = plt.subplots(1, 1, figsize = (8,6))
-plt.plot(results['sol_val_Up_tracker'][-1], label=r"$Up_{%d}$"%(j+1))
-plt.xlabel("Iteration")
-plt.ylabel(r"$Up$")
-plt.title(r"Trace Plot of Up")
-# axes.get_legend().remove()
-legend = plt.legend(bbox_to_anchor=(1.05, 0.5), loc="center left", borderaxespad=0)
-fig.tight_layout()
-plt.subplots_adjust(right=0.7)
-fig.savefig(plotdir +'Up.png', bbox_extra_artists=(legend,), bbox_inches='tight', dpi = 100)
-plt.close()
-
-fig, axes = plt.subplots(1, 1, figsize = (8,6))
-plt.plot(results['sol_val_Z_tracker'][-1], label=r"$Z_{%d}$"%(j+1))
-plt.xlabel("Iteration")
-plt.ylabel(r"$Z$")
-plt.title(r"Trace Plot of Z")
-# axes.get_legend().remove()
-legend = plt.legend(bbox_to_anchor=(1.05, 0.5), loc="center left", borderaxespad=0)
-fig.tight_layout()
-plt.subplots_adjust(right=0.7)
-fig.savefig(plotdir +'Z.png', bbox_extra_artists=(legend,), bbox_inches='tight', dpi = 100)
-plt.close()
-
 size = results['size']
 for j in range(size):
-    # For theta parameters
+    # for i in range(len(results['sol_val_Um_tracker'])):
+        i = len(results['sol_val_Up_tracker'])-1
+        fig, axes = plt.subplots(1, 1, figsize = (8,6))
+        plt.plot(results['sol_val_Um_tracker'][i][j,:], label=r"site_%d_iter_%d"%(j+1, i+1))
+        plt.xlabel("Iteration")
+        plt.ylabel(r"$Um$")
+        plt.title(r"Trace Plot of Um for site_%d_iter_%d"%(j+1, i+1))
+        legend = plt.legend(bbox_to_anchor=(1.05, 0.5), loc="center left", borderaxespad=0)
+        fig.tight_layout()
+        plt.subplots_adjust(right=0.7)
+        fig.savefig(plotdir +'Um_site_%d_iter_%d.png'%(j+1, i+1), bbox_extra_artists=(legend,), bbox_inches='tight', dpi = 100)
+        plt.close()
+
+# # For Up
+for j in range(size):
+    # for i in range(len(results['sol_val_Up_tracker'])):
+        i = len(results['sol_val_Up_tracker'])-1
+        fig, axes = plt.subplots(1, 1, figsize = (8,6))
+        plt.plot(results['sol_val_Up_tracker'][i][j,:], label=r"$Up_{site_%d, iter_%d}$"%(j+1, i))
+        plt.xlabel("Iteration")
+        plt.ylabel(r"$Up$")
+        plt.title(r"Trace Plot of Up")
+        legend = plt.legend(bbox_to_anchor=(1.05, 0.5), loc="center left", borderaxespad=0)
+        fig.tight_layout()
+        plt.subplots_adjust(right=0.7)
+        fig.savefig(plotdir + 'Up_site_%d_iter_%d.png'%(j+1, i), bbox_extra_artists=(legend,), bbox_inches='tight', dpi = 100)
+        plt.close()
+
+# For Z
+for j in range(size):
+    # for i in range(len(results['sol_val_Z_tracker'])):
+        i = len(results['sol_val_Z_tracker'])-1
+        fig, axes = plt.subplots(1, 1, figsize = (8,6))
+        plt.plot(results['sol_val_Z_tracker'][i][j,:], label=r"$Z_{site_%d, iter_%d}$"%(j+1, i))
+        plt.xlabel("Iteration")
+        plt.ylabel(r"$Z$")
+        plt.title(r"Trace Plot of Z")
+        legend = plt.legend(bbox_to_anchor=(1.05, 0.5), loc="center left", borderaxespad=0)
+        fig.tight_layout()
+        plt.subplots_adjust(right=0.7)
+        fig.savefig(plotdir + 'Z_site_%d_iter_%d.png'%(j+1, i), bbox_extra_artists=(legend,), bbox_inches='tight', dpi = 100)
+        plt.close()
+
+
+size = results['size']
+
+for j in range(size):
+
+    num_samples = len(results['collected_ensembles'][len(results['collected_ensembles'])-1][:, j])
+    mu = theta[j]  # Assuming theta is your mean array
+    sigma = thetaSD[j]  # Assuming thetaSD is your standard deviation array
+    theta_samples = np.random.normal(mu, sigma, num_samples)
+
+    mu = gamma[j]  # Assuming gamma is your mean array
+    sigma = gammaSD[j]  # Assuming gammaSD is your standard deviation array
+    gamma_samples = np.random.normal(mu, sigma, num_samples)
+    
+    # Find min and max across all iterations for the same site
+    min_theta = np.min([np.min(results['collected_ensembles'][i][:, j]) for i in range(len(results['collected_ensembles']))]+ [np.min(theta_samples)])
+    max_theta = np.max([np.max(results['collected_ensembles'][i][:, j]) for i in range(len(results['collected_ensembles']))]+ [np.max(theta_samples)])
+
+    min_gamma = np.min([np.min(results['collected_ensembles'][i][:, j + size]) for i in range(len(results['collected_ensembles']))]+ [np.min(gamma_samples)])
+    max_gamma = np.max([np.max(results['collected_ensembles'][i][:, j + size]) for i in range(len(results['collected_ensembles']))]+ [np.max(gamma_samples)])
+
+
+    # for i in range(len(results['collected_ensembles'])):
+    i = len(results['sol_val_Z_tracker'])-1
+        # For theta parameters
     fig, axes = plt.subplots(1, 1, figsize = (8,6))
-    sns.histplot(results['collected_ensembles'][0][:, j], bins=100, label="Unadjusted", kde=False, color='blue')
-    sns.histplot(results['collected_ensembles'][len(results['collected_ensembles'])-1][:, j], bins=100, label="Adjusted", kde=False, color='red')
-    plt.xlabel(r"$\theta_{%d}$"%(j+1))
+    sns.histplot(theta_samples, bins=100, label="Unadjusted", kde=False, color='blue')
+    sns.histplot(results['collected_ensembles'][i][:, j], bins=100, label="Adjusted", kde=False, color='red')
+    
+    plt.xlim([min_theta, max_theta])  # Set x-axis limits
+
+    plt.xlabel(r"$\theta_{site_%d}$"%(j+1))
     plt.ylabel("Distribution")
-    plt.title(r"Distribution of $\theta_{%d}$"%(j+1))
+    plt.title(r"Distribution of $\theta_{site_%d}$ for iteration %d"%(j+1, i))
     plt.legend()
-    fig.savefig(plotdir + 'theta_%d.png'%(j+1), dpi = 100)
+    fig.savefig(plotdir + 'theta_site_%d_iter_%d.png'%(j+1, i), dpi = 100)
     plt.close()
 
     # For gamma parameters
     fig, axes = plt.subplots(1, 1, figsize = (8,6))
-    sns.histplot(results['collected_ensembles'][0][:, j + size], bins=100, label="Unadjusted", kde=False, color='blue')
-    sns.histplot(results['collected_ensembles'][len(results['collected_ensembles'])-1][:, j + size], bins=100, label="Adjusted", kde=False, color='red')
-    plt.xlabel(r"$\gamma_{%d}$"%(j+1))
-    plt.ylabel("Distribution")
-    plt.title(r"Distribution of $\gamma_{%d}$"%(j+1))
-    plt.legend()
-    fig.savefig(plotdir + 'gamma_%d.png'%(j+1), dpi = 100)
-    plt.close()
+    sns.histplot(gamma_samples, bins=100, label="Unadjusted", kde=False, color='blue')
+    sns.histplot(results['collected_ensembles'][i][:, j + size], bins=100, label="Adjusted", kde=False, color='red')
+    plt.xlim([min_gamma, max_gamma])  # Set x-axis limits
 
+    plt.xlabel(r"$\gamma_{site_%d}$"%(j+1))
+    plt.ylabel("Distribution")
+    plt.title(r"Distribution of $\gamma_{site_%d}$ for iteration %d"%(j+1, i))
+    plt.legend()
+    fig.savefig(plotdir + 'gamma_site_%d_iter_%d.png'%(j+1, i), dpi = 100)
+    plt.close()
